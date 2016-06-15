293b49470544ecfa88b50d0537607de7a25a227b
Suriyan Laohaprapanon <suriyant@gmail.com>
2016-06-15 11:41:15 +0700

    new script using lxml that optimize for speed and memory requirement.

---
+++ scripts/README.md
@@ -0,0 +1,44 @@
+DMOZ Domain categorize to CSV script
+====================================
+
+Parses Open Directory RDF Dump content (from http://rdf.dmoz.org/) and grouping the categories by domain name.
+
+Pre-requisites
+--------------
+- 16GB memory computer/instance is prefered to process large RDF XML content.
+- Python 2.7.x
+- Install additional Python's packages 
+
+  ```pip install -r requirements.txt```
+
+
+Usage
+-----
+
+```
+usage: dmozcat2csv.py [-h] [-v] [--version] [-o OUTPUT] input
+
+positional arguments:
+  input                 RDF file name (XML format)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose
+  --version             show program's version number and exit
+  -o OUTPUT, --output OUTPUT
+                        CSV output filename
+
+```
+
+Example
+-------
+
+```python dmozcat2csv.py content.rdf.u8 -o output.csv```
+
+Output format
+-------------
+
+CSV file with the following columns :-
+
+| domain name | category1 | category2 | category3 | ... | categoryN |
+| --- | --- | --- | --- | --- | --- |
---
+++ scripts/dmozcat2csv.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import csv
+
+import tldextract
+from lxml import etree
+
+__version__ = '0.1.0'
+
+NS = {'dmoz_rdf': 'http://dmoz.org/rdf/'}
+
+DEF_OUT_CSV = 'output.csv'
+
+LOG_FILE = "dmozcat2csv.log"
+
+
+def setup_logger(level=logging.DEBUG):
+    """ Set up logging
+    """
+    logging.basicConfig(level=level,
+                        format='%(asctime)s  %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename=LOG_FILE,
+                        filemode='a')
+    console = logging.StreamHandler()
+    console.setLevel(level)
+    formatter = logging.Formatter('%(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s ' + __version__)
+    parser.add_argument('input', help='RDF file name (XML format)')
+    parser.add_argument('-o', '--output', action='store', dest='output',
+                        default=DEF_OUT_CSV, help='CSV output filename')
+
+    results = parser.parse_args()
+    return results
+
+
+def get_topic_list(external_page):
+    topics = set()
+    for topic in external_page.findall('dmoz_rdf:topic', NS):
+        topic_text = topic.text
+        topics.add(topic_text)
+
+    return topics
+
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.verbose:
+        setup_logger(logging.DEBUG)
+    else:
+        setup_logger(logging.INFO)
+
+    logging.debug("input RDF file = '{0!s}'".format(args.input))
+
+    domain_category_dict = {}
+    tree = etree.parse(args.input)
+    root = tree.getroot()
+    for external_page in root.findall('dmoz_rdf:ExternalPage', NS):
+        new_topic_set = get_topic_list(external_page)
+        page_link = external_page.attrib
+        url_string = page_link.get('about')
+        tld = tldextract.extract(url_string)
+        domain = tld.registered_domain
+        if domain == '':
+            logging.warn("Cannot extract the domain from <{0!s}>"
+                         .format(url_string))
+            continue
+        if domain not in domain_category_dict:
+            domain_category_dict[domain] = new_topic_set
+        else:
+            domain_category_dict[domain].update(new_topic_set)
+
+    with open(args.output, 'wb') as f:
+        writer = csv.writer(f)
+        for d in domain_category_dict:
+            cols = [d] + list(domain_category_dict[d])
+            utf8_cols = [x.encode('utf-8') for x in cols]
+            writer.writerow(utf8_cols)
